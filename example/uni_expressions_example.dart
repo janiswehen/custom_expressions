0aecd7035ee968a8a661e567cf0fbcc08196504a
Janis Wehen <janiswehen@gmail.com>
2025-09-19 00:33:42 +0200

    adjust example

--- example/uni_expressions_example.dart
+++ example/uni_expressions_example.dart
@@ -3,7 +3,7 @@ import 'package:uni_expressions/uni_expressions.dart';
 void main() {
   final parser = ExpressionParser();
 
-  final expressionText = '';
+  final expressionText = '1 + 2 * 3';
 
   final expression = parser.parse(expressionText);
 
